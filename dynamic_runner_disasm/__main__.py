1d811751dd12735c98790eb14e100af171945a98
sirati <git@sirati.me>
2026-04-29 02:04:13 +0200

    rename dynamic_batch_disasm to dynamic_runner_disasm
    Signed-off-by: sirati <git@sirati.me>

--- dynamic_runner_disasm/__main__.py
+++ dynamic_runner_disasm/__main__.py
@@ -3,7 +3,7 @@ the same `dynamic_batch.run` facade and `make_subprocess_spawn_factory`
 helper that the tokenizer package uses, with a totally different task.
 """
 
-from dynamic_batch import make_subprocess_spawn_factory, run
+from dynamic_runner import make_subprocess_spawn_factory, run
 
 from .disasm_task import DisasmTask
 
@@ -11,7 +11,7 @@ from .disasm_task import DisasmTask
 def main() -> None:
     run(
         task=DisasmTask(),
-        spawn_secondary_factory=make_subprocess_spawn_factory("dynamic_batch_disasm"),
+        spawn_secondary_factory=make_subprocess_spawn_factory("dynamic_runner_disasm"),
         description="Acceptance-test sibling task package: hypothetical disassembler",
     )
 
